25a888df8a2a9b2a818294538ced49ddd9df78bf
JellyfishDotcom <cshv.dracma@outlook.com>
2022-11-22 22:05:39 -0600

    trajectory_demo_agnes

--- drivers/planner/planner.py
+++ drivers/planner/planner.py
@@ -1,4 +1,53 @@
+import numpy as np
+class Planner:
+    def __init__(self):
+        pass
+    
+    def plan_j3(self, tf, dt, q0, qf, v0, vf, a0, af):
+        q0 = np.array(q0)
+        qf = np.array(qf)
+        v0 = np.array(v0)
+        vf = np.array(vf)
+        a0 = np.array(a0)
+        af = np.array(af)
+        
+        #Time vector
+        t = np.arange(0,tf+dt,dt)
+        
+        # Polynomial coefficients
+        c0 = q0
+        c1 = v0
+        c2 = a0/2
+        c3 = ((20*qf-20*q0)-(8*vf+12*v0)*tf+(af-3*a0)*tf**2)/(2*tf**3)
+        c4 = -((30*qf-30*q0)-(14*vf+16*v0)*tf+(2*af-3*a0)*tf**2)/(2*tf**4)
+        c5 = ((12*qf-12*q0)-(6*vf+6*v0)*tf+(af-a0)*tf**2)/(2*tf**5)
+        
+        # Trajectory
+        q = np.dot(np.array([t**0, t, t**2, t**3, t**4, t**5]).transpose(),np.array([c0, c1, c2, c3, c4, c5]))
+        v = np.dot(np.array([t**0 , t, t**2, t**3, t**4]).transpose(), np.array([c1, 2*c2, 3*c3, 4*c4, 5*c5]))
+        a = np.dot(np.array([t**0, t, t**2, t**3]).transpose(), np.array([2*c2, 6*c3, 12*c4, 20*c5]))
 
+        return a, v, q
 
+if __name__ == '__main__':
+    # Initial and final values
+    q0 = np.array([0])
+    qf = np.array([50])
+    v0 = np.zeros(len(q0))
+    vf = np.zeros(len(q0))
+    a0 = np.zeros(len(q0))
+    af = np.zeros(len(q0)) 
+    tf = 5
 
+    planner = Planner()
+    t, a, v, q = planner.plan_j3(tf, 0.01, q0, qf, v0, vf, a0, af)
 
+    # plotting
+    import matplotlib.pyplot as plt 
+    plt.plot(t,q,label='Position')
+    plt.plot(t, v, label='Velocity')
+    plt.plot(t, a, label='Acceleration')
+    plt.legend()
+    plt.grid()
+    plt.title('Joint position (5th degree polynomial)')
+    plt.show() 
---
+++ trajectory_demo_agnes.py
@@ -0,0 +1,70 @@
+import pybullet as pb
+import pybullet_data
+from drivers.solver.solver_agnes import Ik_solver_agnes
+from drivers.planner.planner import Planner
+import time
+import numpy as np
+
+class Agnes(Ik_solver_agnes, Planner):
+    def __init__(self, robot, dt, d1, a2, d4, d6):
+        super().__init__(d1, a2, d4, d6)
+
+        self._robot = robot
+        self._dt = dt
+        self.gripper_state = False
+        self.joint_state = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+        
+    def set_joints(self, joint_state):
+        pb.setJointMotorControlArray(self._robot,
+                                 range(pb.getNumJoints(self._robot)),
+                                 pb.POSITION_CONTROL,
+                                 targetPositions = self.joint_state,
+                                 forces = [100, 100, 100, 100, 100, 100])
+        self.joint_state = joint_state
+        
+    def move(self, target_joint_state, duration):
+        _, _, path = self.plan_j3(duration, 
+                                 self._dt, 
+                                 self.joint_state, 
+                                 target_joint_state,
+                                 len(self.joint_state)*[0],
+                                 len(self.joint_state)*[0],
+                                 len(self.joint_state)*[0],
+                                 len(self.joint_state)*[0])
+        
+        input('Press Enter to continue...')
+        for i in range(path.shape[0]):
+            self.set_joints(path[i])
+            time.sleep(self._dt)
+            pb.stepSimulation()
+            
+if __name__ == '__main__':
+    # Make an instance of a physic client
+    py_client = pb.connect(pb.GUI)
+
+    # Simulation parameters
+    pb.setGravity(0, 0, -9.81)
+    pb.setRealTimeSimulation(0)
+
+    # Objects
+    pb.setAdditionalSearchPath(pybullet_data.getDataPath())
+    ground = pb.loadURDF('plane.urdf')
+    #table = pb.loadURDF('table/table.urdf') 
+    cube = pb.loadURDF('cube.urdf', basePosition = [1.75, 0.0, 0.25], globalScaling = 0.5)
+    agnes_urdf = pb.loadURDF('/URDF/agnes.urdf.xml', useFixedBase=1)
+    
+    # Instanciating an Agnes robot
+    agnes = Agnes(agnes_urdf, 0.01, 0.485, 1.0, 0.74, 0.254374022)
+    
+    # Movement sequence
+    states = (((1.5, 0.25, 0.5),(-np.pi, -np.pi/4, -np.pi/4)),
+              ((1.5, -0.25, 0.5), (-np.pi, -np.pi/4, np.pi/4)))       
+
+    for i, state in enumerate(states):
+        q = agnes.solve(state[0], state[1],-1)
+        print(f'------ q {type(q)}')
+        agnes.move(q, 10)
+    
+    # End program
+    input('Press ENTER to stop...')
+    pb.disconnect()
