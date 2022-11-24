2058d69bbf78ebf52d080be03df19073bbf0ebd6
JellyfishDotcom <cshv.dracma@outlook.com>
2022-11-24 15:48:37 -0600

    corrección temporal

--- drivers/planner/planner.py
+++ drivers/planner/planner.py
@@ -32,25 +32,19 @@ class Planner:
     
     def plan_line(self, tf, dt, p0, pf):
         dt = int(tf/dt)
-        
         # Position and orientation
         o0 = np.array(p0[:3])
         of = np.array(pf[:3])
-        
         a0 = np.array(np.deg2rad(p0[3:6]))
         af = np.array(np.deg2rad(pf[3:6]))
-        
         # Auxiliar vector coefficients
         v = of - o0
-        
         # x-vector and time vector
         x = np.linspace(p0[0], pf[0], dt)
         t = (x - p0[0])/ v[0]
-        
         # y and z vectors
         y = p0[1] + v[1]*t
         z = p0[2] + v[2]*t
-        
         # Wrist
         a = np.linspace(p0[3], pf[3], dt)
         b = np.linspace(p0[4], pf[4], dt)
@@ -67,9 +61,11 @@ if __name__ == '__main__':
     a0 = np.zeros(len(q0))
     af = np.zeros(len(q0)) 
     tf = 5
+    dt = 0.01
 
     planner = Planner()
-    t, a, v, q = planner.plan_j3(tf, 0.01, q0, qf, v0, vf, a0, af)
+    a, v, q = planner.plan_j3(tf, 0.01, q0, qf, v0, vf, a0, af)
+    t = np.arange(0,tf+dt,dt)
 
     # plotting
     import matplotlib.pyplot as plt 
